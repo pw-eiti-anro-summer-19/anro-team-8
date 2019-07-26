71e5b7f0fff7d726bb565b76372467c921f67281
Your Name <you@example.com>
2019-07-27 00:59:45 +0800

    lab 4

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -37,17 +37,22 @@
 ## to the 'chatter' topic
 
 import rospy
-from std_msgs.msg import String
+import math
+from sensor_msgs.msg import JointState
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    d = 0.0
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        msg = JointState()
+        msg.header.stamp = rospy.get_rostime()
+        msg.name = ['jiont_0', 'joint_1', 'joint_2']
+        msg.position = [math.sin(d),math.sin(d),math.sin(d)]
+        pub.publish(msg)
         rate.sleep()
+        d = d+0.1
 
 if __name__ == '__main__':
     try:
