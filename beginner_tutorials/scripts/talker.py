0cc9b5aaa59979fba575a5ddb6040b38608b8509
Your Name <you@example.com>
2019-07-30 11:07:34 +0200

    new commit

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -43,16 +43,32 @@ from sensor_msgs.msg import JointState
 def talker():
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 20hz
-    d= 0.0
-    while not rospy.is_shutdown():
+    hz = 10
+    rate = rospy.Rate(hz) # 10hz
+    d=0.0
+    theta1 = 1.5
+    theta2 = 1.5
+    theta3 = 1.5
+    time = 5
+    movement_cal = time * hz
+    theta1_new = 0
+    theta2_new = 0
+    theta3_new = 0
+    theta_tem1 = theta1 / movement_cal
+    theta_tem2 = theta2 / movement_cal
+    theta_tem3 = theta3 / movement_cal
+
+    while movement_cal > 0:
         msg = JointState()
+        theta1_new = theta1_new + theta_tem1
+        theta2_new = theta2_new + theta_tem2
+        theta3_new = theta3_new + theta_tem3
         msg.header.stamp= rospy.get_rostime()
         msg.name = ['base_to_link_1', 'base_to_link_2', 'base_to_link_3']
-        msg.position = [math.sin(d),0,math.sin(d)]
+        msg.position = [0,0,theta3_new]
         pub.publish(msg)
         rate.sleep()
-        d= d+0.1
+        movement_cal = movement_cal - 1
 
 if __name__ == '__main__':
     try:
